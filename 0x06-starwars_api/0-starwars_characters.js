1dac996d0b42d81cd38c40d503559cf69c7aa3ad
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-08-11 09:17:47 +0300

    Star wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,60 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function getMovieCharacters (movieId) {
+  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+  return new Promise((resolve, reject) => {
+    request.get(filmUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const filmData = JSON.parse(body);
+        resolve(filmData.characters);
+      }
+    });
+  });
+}
+
+function getCharacterName (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
+}
+
+function printCharacters (movieId) {
+  getMovieCharacters(movieId)
+    .then(characters => {
+      if (characters.length === 0) {
+        console.log('No characters found for the given movie ID.');
+        return;
+      }
+
+      console.log(`Characters in the movie (ID ${movieId}):`);
+      Promise.all(characters.map(getCharacterName))
+        .then(characterNames => {
+          characterNames.forEach(name => console.log(name));
+        })
+        .catch(error => {
+          console.error('Error fetching character names:', error);
+        });
+    })
+    .catch(error => {
+      console.error('Error fetching movie characters:', error);
+    });
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: ./script.js <movie_id>');
+} else {
+  printCharacters(movieId);
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0x06-starwars_api
+
+a script that prints all characters of a Star Wars movie
